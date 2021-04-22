8a0b26898ee3f7315de33a83b3763bbdb3e228e2
Manali Jain <Manali.Jain@kanini.com>
2021-04-22 16:52:38 +0530

    Added NUnit test

--- Repository/BenchMarkRepo.cs
+++ Repository/BenchMarkRepo.cs
@@ -8,27 +8,33 @@ namespace AuditBenchmarkAPI.Repository
 {
     public class BenchMarkRepo : IBenchMarckRepo
     {
-        Dictionary<string,int> internalandsoxdict = new Dictionary<string,int>();
+        Dictionary<string,int> _internalandsoxdict = new Dictionary<string,int>();
 
+        public BenchMarkRepo()
+        {
 
+        }
+
+        public BenchMarkRepo(Dictionary<string,int> internalandsoxDict)
+        {
+            _internalandsoxdict = internalandsoxDict;
+        }
 
         public Dictionary<string,int> GetInternaAndSOXNoCount(string auditType)
         {
 
-
-
             if (auditType == "Internal")
             {
-                internalandsoxdict.Add("Internal", 3);
+                _internalandsoxdict.Add("Internal", 3);
             }
             else {
                 if(auditType == "SOX")
                 {
-                    internalandsoxdict.Add("SOX", 1);
+                    _internalandsoxdict.Add("SOX", 1);
                 }
             }
 
-            return internalandsoxdict;
+            return _internalandsoxdict;
                 
 
         }
